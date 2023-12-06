1a3400bf3896783d43db18300c0098f37a7860dd
Franck Silvestre <franck.silvestre@gmail.com>
2023-12-06 11:24:40 +0100

    Suppression of deprecated annotation

--- src/test/java/org/example/RunCucumberTest.java
+++ src/test/java/org/example/RunCucumberTest.java
@@ -1,6 +1,13 @@
 package org.example;
 
-import io.cucumber.junit.platform.engine.Cucumber;
 
-@Cucumber
+import org.junit.platform.suite.api.IncludeClassNamePatterns;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("Cucumber tests")
+@SelectPackages("org.example")
+@IncludeClassNamePatterns(".*Stepdefs")
 public class RunCucumberTest {}
